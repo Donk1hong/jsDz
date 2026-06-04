61dd166ae3350262a859407f867d16adb9e147d3
Donk1hong <roma1324465@gmail.com>
2026-06-04 03:04:22 +0300

    fix bug

--- crypto.js
+++ crypto.js
@@ -1,21 +1,19 @@
 function crypto(password) {
-    let chars = password.split('');
+    let mid = Math.floor(password.length / 2);
 
-    for (let i = 0; i < chars.length - 1; i += 2) {
-        let temp = chars[i];
-        chars[i] = chars[i + 1];
-        chars[i + 1] = temp;
-    }
+    let firstHalf = password.slice(0, mid);
+    let secondHalf = password.slice(mid);
 
-    return chars.join('');
+    let revFirst = firstHalf.split('').reverse().join('');
+    let revSecond = secondHalf.split('').reverse().join('');
+
+    return revFirst + revSecond;
 }
 
 function check(encrypted, password) {
-    let decrypted = crypto(encrypted);
-
-    return decrypted === password;
+    return crypto(encrypted) === password;
 }
 
 console.log(crypto('password'));
 console.log(check('apssowdr', 'password'));
-console.log(check('ssapdorw', 'wrong'));
+console.log(check('ssapdrow', 'password'));
