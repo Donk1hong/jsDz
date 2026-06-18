50d5e9d2ab9c1e3e155adb5d8559ed4ecd45da05
Donk1hong <roma1324465@gmail.com>
2026-06-19 01:51:03 +0300

    skip

---
+++ sort-loops.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+const arr = [1, 40, -5, 10, 0];
+console.log(bubbleSort(arr));
