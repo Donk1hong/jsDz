c323c07cd4ea71ff29e137e53f84fb3958534afc
rus <roma1324465@gmail.com>
2026-09-24 15:34:45 +0300

    boardGames

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 "use strict";
 
-const users = [
-  { id: 1, name: "Вася" },
-  { id: 2, name: "Петя" },
-  { id: 1, name: "Вася" },
-];
+function boardGames(dice) {
+  if (!dice) {
+    console.log("Вы не передали значение!");
+    return undefined;
+  }
+  const cubes = ["d4", "d6", "d8", "d10", "d12", "d16", "d20"];
+  const finCube = cubes.find((cube) => cube === dice);
+  if (!finCube) {
+    console.log("Такого кубика не существует");
+    return undefined;
+  }
+  const cubeConversion = Number.parseInt(dice.slice(1));
+  const randomFace = Math.floor(Math.random() * (cubeConversion - 1 + 1) + 1);
+  console.log(randomFace);
+  return randomFace;
+}
 
-const idUsers = users.map((item) => item.id);
-const setUsers = new Set(idUsers);
-const setUserArr = [...setUsers];
-const result = setUserArr.map((id) => {
-  return users.find((user) => user.id === id);
-});
-
-console.log(result);
+boardGames("d20");
